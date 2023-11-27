0203bf0f68d7650a9644319b85b080ecdba52fa7
YonatanEliyahu <106242827+YonatanEliyahu@users.noreply.github.com>
2023-11-27 11:15:45 +0200

    UPDATE: server_TCP.py

--- server_TCP.py
+++ server_TCP.py
@@ -1,35 +1,145 @@
+##############################################################################
+# server.py
+##############################################################################
+
 import socket
-import select
 import chatlib
 
-SERVER_IP = "0.0.0.0"
-SERVER_PORT = 5678
-MSG_MAX_SIZE = 4096
+# GLOBALS
+users = {}
+questions = {}
+logged_users = {} # a dictionary of client hostnames to usernames - will be used later
 
-def print_client_sockets(client_sockets:list):
-    for c in client_sockets:
-        print('\t',c.getpeername())
+ERROR_MSG = "Error! "
+SERVER_PORT = 5678
+SERVER_IP = "127.0.0.1"
+
+
+# HELPER SOCKET METHODS
+
+def build_and_send_message(conn, code, msg):
+	## copy from client
+	
+	print("[SERVER] ",full_msg)	  # Debug print
+
+def recv_message_and_parse(conn):
+	## copy from client
+	
+	print("[CLIENT] ",full_msg)	  # Debug print
+	
+
+
+# Data Loaders #
+
+def load_questions():
+	"""
+	Loads questions bank from file	## FILE SUPPORT TO BE ADDED LATER
+	Recieves: -
+	Returns: questions dictionary
+	"""
+	questions = {
+				2313 : {"question":"How much is 2+2","answers":["3","4","2","1"],"correct":2},
+				4122 : {"question":"What is the capital of France?","answers":["Lion","Marseille","Paris","Montpellier"],"correct":3} 
+				}
+	
+	return questions
+
+def load_user_database():
+	"""
+	Loads users list from file	## FILE SUPPORT TO BE ADDED LATER
+	Recieves: -
+	Returns: user dictionary
+	"""
+	users = {
+			"test"		:	{"password":"test","score":0,"questions_asked":[]},
+			"yossi"		:	{"password":"123","score":50,"questions_asked":[]},
+			"master"	:	{"password":"master","score":200,"questions_asked":[]}
+			}
+	return users
+
+	
+# SOCKET CREATOR
+
+def setup_socket():
+	"""
+	Creates new listening socket and returns it
+	Recieves: -
+	Returns: the socket object
+	"""
+	# Implement code ...
+	
+	return sock
+	
+
+
+		
+def send_error(conn, error_msg):
+	"""
+	Send error message with given message
+	Recieves: socket, message error string from called function
+	Returns: None
+	"""
+	# Implement code ...
+	
+
+
+	
+##### MESSAGE HANDLING
+
+
+def handle_getscore_message(conn, username):
+	global users
+	# Implement this in later chapters
+
+	
+def handle_logout_message(conn):
+	"""
+	Closes the given socket (in laster chapters, also remove user from logged_users dictioary)
+	Recieves: socket
+	Returns: None
+	"""
+	global logged_users
+	
+	# Implement code ...
+
+
+def handle_login_message(conn, data):
+	"""
+	Gets socket and message data of login message. Checks  user and pass exists and match.
+	If not - sends error and finished. If all ok, sends OK message and adds user and address to logged_users
+	Recieves: socket, message code and data
+	Returns: None (sends answer to client)
+	"""
+	global users  # This is needed to access the same users dictionary from all functions
+	global logged_users	 # To be used later
+	
+	# Implement code ...
+
+
+def handle_client_message(conn, cmd, data):
+	"""
+	Gets message code and data and calls the right function to handle command
+	Recieves: socket, message code and data
+	Returns: None
+	"""
+	global logged_users	 # To be used later
+	
+	# Implement code ...
+	
 
 
 def main():
-    print("Setting up server...")
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((SERVER_IP, SERVER_PORT))
-    server_socket.listen()
-    print("Server is ready and listening to new clients...")
-    client_sockets = []  # list of connected clients
+	# Initializes global users and questions dicionaries using load functions, will be used later
+	global users
+	global questions
+	
+	print("Welcome to Trivia Server!")
+	
+	# Implement code ...
 
-    while True:
-        ready_to_read, ready_to_write, in_error = select.select([server_socket] + client_sockets, [], [])
-        for current_socket in ready_to_read:
-            if current_socket is server_socket:
-                client_socket, client_address = current_socket.accept()
-                print(f"New client joined {client_address}")
-                client_sockets.append(client_socket)
-            else:
-                # respond
-                pass
-            print_client_sockets(client_sockets)
+
 
 if __name__ == '__main__':
-    main()
+	main()
+
+	
